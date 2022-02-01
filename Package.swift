88266ef81eee65af2ef384a7493963290a54bde5
Muzammil Peer <muzammilpeer987@gmail.com>
2022-02-01 22:10:33 +0400

    platforms: [.iOS(.v10), .macOS(.v10_13), .tvOS(.v10), .watchOS(.v2)],

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "SharedModule",
+    platforms: [.iOS(.v10), .macOS(.v10_13), .tvOS(.v10), .watchOS(.v2)],
+
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
