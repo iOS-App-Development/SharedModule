89355e614869e9507ecabbc9fd4194298bdd0713
Muzammil Peer <muzammilpeer987@gmail.com>
2022-02-17 01:13:15 +0400

    imports added

--- Sources/SharedModule/SharedModule.swift
+++ Sources/SharedModule/SharedModule.swift
@@ -1,6 +1,17 @@
+import Foundation
+import APIClient
+
 public struct SharedModule {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+}
+extension SharedModule:SharedModuleAPI
+{
+    func load() {
+    }
+    
+    func unload() {
     }
 }
---
+++ Sources/SharedModule/SharedModuleAPI.swift
@@ -0,0 +1,14 @@
+//
+//  SharedModuleAPI.swift
+//  
+//
+//  Created by Muzammil Peer on 17/02/2022.
+//
+
+
+import Foundation
+
+protocol SharedModuleAPI {
+    func load()
+    func unload()
+}
